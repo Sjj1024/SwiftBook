358669f384d2b0a6fd6ed03133061faeb511ea3a
1024小神 <15670339118@163.com>
2024-07-02 19:42:15 +0800

    add gitignore

---
+++ SwiftBook/SwiftBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBookApp.swift
+//  SwiftBook
+//
+//  Created by song on 2024/7/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
