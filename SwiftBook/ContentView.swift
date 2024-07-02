b6fedf1ab4962265dbfc3258451d3c6ec2cf1546
1024小神 <15670339118@163.com>
2024-07-02 22:41:00 +0800

    add some field

--- SwiftBook/ContentView.swift
+++ SwiftBook/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = ""
+
+    @State var password = ""
+
+    @State var info = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField(text: $name, label: {
+                Text("用户名")
+            })
+            SecureField(text: $password, label: {
+                Text("密码")
+            })
+            TextEditor(text: $info)
         }
         .padding()
     }
