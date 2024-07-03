37cf753ac6def5b7108af8578a522c4809380c79
1024小神 <15670339118@163.com>
2024-07-03 19:55:21 +0800

    add button

--- SwiftBook/ContentView.swift
+++ SwiftBook/ContentView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct ContentView: View {
     @State var name = ""
     @State var password = ""
+    @State var gender = ""
+    @State var dataList = ["男", "女", "其他"]
+    @State var timer = Date()
+    @State var voice = 0.0
+    @State var remember = true
     @State var info = "简介"
 
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Image("desktop")
                 .resizable()
                 .frame(width: .infinity, height: 260)
@@ -24,6 +29,31 @@ struct ContentView: View {
             }).padding()
             SecureField(text: $password, label: {
                 Text("密码")
+            }).padding()
+            // 选择
+            Label(
+                title: { Text(gender) },
+                icon: { Image(systemName: "person") }
+            ).padding()
+            Picker(selection: $gender, content: {
+                ForEach(dataList, id: \.self) { item in
+                    Text(item)
+                }
+            }) {
+                Text("性别")
+            }
+            // 出生日期
+            DatePicker(selection: $timer, displayedComponents: DatePickerComponents.date, label: {
+                Text("出生日期:\(timer)")
+            }).padding()
+            // 声音大小
+            HStack {
+                Text("声音: \(voice)")
+                Slider(value: $voice, in: -10 ... 10).tint(.red)
+            }.padding()
+            // 记住密码
+            Toggle(isOn: $remember, label: {
+                Text("记住密码")
             }).padding()
             TextEditor(text: $info)
                 .padding()
